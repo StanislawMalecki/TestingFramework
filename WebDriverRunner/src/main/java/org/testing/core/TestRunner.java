73f727481b1c5da4f9cddec51169beeb97980044
StanisławMałecki <stas.malecki323@gmail.com>
2024-01-21 17:41:48 +0100

    runner multiple tests from environmental variables works

--- WebDriverRunner/src/main/java/org/testing/core/TestRunner.java
+++ WebDriverRunner/src/main/java/org/testing/core/TestRunner.java
@@ -2,7 +2,6 @@ package org.testing.core;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.platform.commons.util.StringUtils;
 import org.junit.platform.engine.DiscoverySelector;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
@@ -12,16 +11,13 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.InvalidParameterException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
